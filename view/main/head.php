a494dbd0e5800eb866a1f4bc12b6ac38734faced
Mohammed Al-Amin <sdwinar@gmail.com>
2023-10-22 23:45:08 +0400

    xse

---
+++ view/main/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- <link rel="shortcut icon" href="media/img/main/suss.ico" /> -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" id="bootstrap-css">
+    <link rel="stylesheet" href="assets/css/all.min.css">
+    <title>
+    <?php
+       echo isset($headname) ? $headname : 'SUSS';
+        ?>
+    </title>
+</head>
